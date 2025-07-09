e3ffb519662e14a5074cba95ee9ba1da4f2d2513
MacTuan007 <2024801030069@student.tdmu.edu.vn>
2025-07-10 04:59:35 +0700

    update

--- src/layoutsAdmin/AdminLayout.tsx
+++ src/layoutsAdmin/AdminLayout.tsx
@@ -4,7 +4,7 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
     return (
         <div className="d-flex" style={{ minHeight: "100vh" }}>
             {/* Sidebar có độ rộng cố định */}
-            <div style={{ width: "220px" }}>
+            <div style={{ width: "220px", marginRight: "20px" }} className="bg-light">
                 <Sidebar />
             </div>
 
