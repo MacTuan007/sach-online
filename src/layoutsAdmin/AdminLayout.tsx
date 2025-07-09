59fbcdc02aaa9ac0664185468fe8f6af0ec35654
MacTuan007 <2024801030069@student.tdmu.edu.vn>
2025-07-10 04:56:41 +0700

    update

--- src/layoutsAdmin/AdminLayout.tsx
+++ src/layoutsAdmin/AdminLayout.tsx
@@ -2,9 +2,14 @@ import Sidebar from "../partialsAdmin/Sidebar";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="d-flex">
-            <Sidebar />
-            <div className="flex-grow-1 p-3" style={{ marginLeft: "250px" }}>
+        <div className="d-flex" style={{ minHeight: "100vh" }}>
+            {/* Sidebar có độ rộng cố định */}
+            <div style={{ width: "220px" }}>
+                <Sidebar />
+            </div>
+
+            {/* Nội dung chính, không bị margin thừa */}
+            <div className="flex-grow-1 px-4 pt-3">
                 {children}
             </div>
         </div>
